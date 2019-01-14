73fdda540532cd03004b7608266d6afaa54b57b0
maxwell.chen <maxwell.chen@titansoft.com.sg>
2019-01-14 20:08:15 +0800

    Refactor

--- BowlingTest/Bowling.cs
+++ BowlingTest/Bowling.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BowlingTest
 {
@@ -7,25 +9,45 @@ namespace BowlingTest
         private int _score;
         private int _ballsCount;
         private int _framesCount;
+        public List<Frame> Frames = new List<Frame>();
+        
+
+        public Bowling()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Frames.Add(new Frame());
+            }
+        }
 
         public void Roll(int score)
         {
             _ballsCount++;
+            IsNewFram();
+            Frames[_framesCount].Score += score;
+        }
+
+        private void IsNewFram()
+        {
             if (_ballsCount % 2 == 0)
             {
                 _framesCount++;
             }
-            _score += score;
         }
 
         public int Score()
         {
-            if (_framesCount == 0)
-            {
-                return 0;
-            }
-
-            return _score;
+            return _framesCount == 0 ? 0 : ComputeScore();
         }
+
+        private int ComputeScore()
+        {
+            return Frames.Sum(x => x.Score);
+        }
+    }
+
+    public class Frame
+    {
+        public int Score;
     }
 }
