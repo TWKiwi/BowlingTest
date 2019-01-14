341a86ce9f2def6ffc2beb17ed5ffa9dc3a02179
maxwell.chen <maxwell.chen@titansoft.com.sg>
2019-01-15 00:07:50 +0800

    RollNineteenBalls_OneStrike pass

--- BowlingTest/Bowling.cs
+++ BowlingTest/Bowling.cs
@@ -5,15 +5,20 @@ namespace BowlingTest
 {
     public class Bowling
     {
-        private int _ballsCount;
         private int _framesCount;
         public List<Frame> Frames = new List<Frame>();
         private int _tempTotalScore;
         private int _spireBonusTimes;
+        private int _strikeBonusTimes;
+        private bool _isFirstBall;
+        private bool _isFrameBonus;
+        private bool _youHaveNoChance;
+
         public int CurrentFrameIndex => _framesCount - 1;
 
         public Bowling()
         {
+            _framesCount++;
             for (int i = 0; i < 10; i++)
             {
                 Frames.Add(new Frame());
@@ -22,24 +27,93 @@ namespace BowlingTest
 
         public void Roll(int score)
         {
-            _ballsCount++;
-            NewFrameCounter();
+            IsReachTheUpperLimit();
+
+            if (_isFrameBonus)
+            {
+                _framesCount--;
+            }
+
+            if (_youHaveNoChance) return;
+            SetUpCounter();
             _tempTotalScore += score;
             HasSpireBonus(score);
-            if (!IsNewFrame())
+            HasStrikeBonus(score);
+
+            if (IsSpire())
+            {
+                _spireBonusTimes++;
+            }
+
+            if (IsStrike())
+            {
+                _strikeBonusTimes += 2;
+                _isFirstBall = false;
+            }
+
+
+            if (!_isFirstBall)
+            {
+                FrameCompleted();
+                SetUpScoreIntoCurrentFrame();
+                _tempTotalScore = 0;
+                _framesCount++;
+                _isFirstBall = false;
+            }
+        }
+
+        private void IsReachTheUpperLimit()
+        {
+            if (CurrentFrameIndex >= 10 && _spireBonusTimes > 0)
+            {
+                _isFrameBonus = true;
+            }
+            else if (CurrentFrameIndex >= 10 && _spireBonusTimes < 0 && _strikeBonusTimes < 0)
+            {
+                _youHaveNoChance = true;
+            }
+        }
+
+        private void FrameCompleted()
+        {
+            Frames[CurrentFrameIndex].IsCompleted = true;
+        }
+
+        private void SetUpScoreIntoCurrentFrame()
+        {
+            if (!FrameUnCompleted())
             {
                 Frames[CurrentFrameIndex].Score += _tempTotalScore;
-                if (IsSpire())
-                {
-                    _spireBonusTimes++;
-                }
             }
-            
+        }
+
+        private bool HasExistCompletedFrame()
+        {
+            return Frames.Any(x => x.IsCompleted);
+        }
+
+        private void HasStrikeBonus(int score)
+        {
+            if (_strikeBonusTimes > 0 && HasExistCompletedFrame())
+            {
+                Frames[CurrentFrameIndex - 1].Score += score;
+                _strikeBonusTimes--;
+            }
+        }
+
+        private bool IsStrike()
+        {
+            return _tempTotalScore == 10 && _isFirstBall;
+        }
+
+        private void SetUpCounter()
+        {
+            _isFirstBall = !_isFirstBall;
         }
 
         private void HasSpireBonus(int score)
         {
-            if (_spireBonusTimes>0)
+            if (_spireBonusTimes > 0)
             {
                 Frames[CurrentFrameIndex - 1].Score += score;
                 _spireBonusTimes--;
@@ -48,21 +122,12 @@ namespace BowlingTest
 
         private bool IsSpire()
         {
-            return Frames[CurrentFrameIndex].Score == 10;
+            return _tempTotalScore == 10 && !_isFirstBall;
         }
 
-        private void NewFrameCounter()
+        private bool FrameUnCompleted()
         {
-            if (IsNewFrame())
-            {
-                _tempTotalScore = 0;
-                _framesCount++;
-            }
-        }
-
-        private bool IsNewFrame()
-        {
-            return _ballsCount % 2 == 1;
+            return !Frames[CurrentFrameIndex].IsCompleted;
         }
 
         private int UpdateTotalScore()
@@ -79,5 +144,6 @@ namespace BowlingTest
     public class Frame
     {
         public int Score;
+        public bool IsCompleted;
     }
 }
