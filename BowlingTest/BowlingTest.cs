722baf5f6c29776be5b878526bcd215d4112de2b
maxwell.chen <maxwell.chen@titansoft.com.sg>
2019-01-15 20:56:40 +0800

    RollTwentyOneBalls_ContainStrikeAndSpare_Ver2 pass

--- BowlingTest/BowlingTest.cs
+++ BowlingTest/BowlingTest.cs
@@ -311,6 +311,19 @@ namespace BowlingTest
             Assert.AreEqual(132, _bowling.Score());
         }
 
+        [TestMethod]
+        public void RollTwentyOneBalls_ContainStrikeAndSpare_Ver2()
+        {
+            SetRollHistory(new List<int>()
+            {
+                10,10,3,7,9,0,5,2,6,4,10,2,3,5,0,10,3,7
+            });
+
+            RollBalls(_rollHistory);
+
+            Assert.AreEqual(143, _bowling.Score());
+        }
+
         private void SetRollHistory(List<int> dictionary)
         {
             _rollHistory = dictionary;
